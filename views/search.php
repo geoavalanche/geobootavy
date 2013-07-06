60b90cb4e2b0bd79aff87c1dfd6af55b856808f4
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2013-07-06 12:16:01 +0200

    override search functionality and related theme changes

---
+++ views/search.php
@@ -0,0 +1,22 @@
+<div id="content">
+	<div class="content-bg">
+		<div class="widget-box">
+			<div class="widget-title">
+				<table class="table" style="margin:0">
+					<tr>
+						<td class="truncate"><i class="icon-search"></i>Search Results</td>
+						<td width="90">
+							<div class="widget-toolbar pull-right btn-group">
+								<a href="javascript:void(0)" class="btn btn-inverse"><i class="icon-chevron-up"></i></a>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div class="widget-content">
+				<?php echo $search_info; ?>
+				<?php echo $search_results; ?>
+			</div>
+		</div>
+	</div>
+</div>
